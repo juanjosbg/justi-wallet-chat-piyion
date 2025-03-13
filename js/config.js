53c82aef8cb243a8b9edaab1c5fe04b98618baf1
juanjosbg <juanjoseborrero95@gmail.com>
2025-03-12 22:51:25 -0500

    added segurity

--- js/config.js
+++ js/config.js
@@ -3,30 +3,33 @@ document.addEventListener("DOMContentLoaded", async () => {
       console.log("Intentando obtener el Company ID...");
   
       const response = await fetch("/api/getCompanyId");
-      console.log("Respuesta recibida:", response);
-  
       if (!response.ok) {
         throw new Error(`Error en la API: ${response.statusText}`);
       }
   
       const data = await response.json();
-      console.log("Datos recibidos:", data);
-  
       const companyId = data.companyId;
+  
       if (!companyId) {
         throw new Error("Company ID no encontrado en la respuesta.");
       }
   
       console.log("Company ID obtenido:", companyId);
   
-      const chatComponent = document.createElement("chat-component-piyion");
-      chatComponent.id = "chatComponent";
-      chatComponent.style.zIndex = "10";
-      chatComponent.setAttribute("position", "left");
-      chatComponent.setAttribute("company", companyId);
+      // Esperar a que el componente se registre en el DOM
+      customElements.whenDefined("chat-component-piyion").then(() => {
+        console.log("Componente de chat definido, agregándolo al DOM...");
   
-      document.body.appendChild(chatComponent);
-      console.log("Chat component añadido al DOM.");
+        const chatComponent = document.createElement("chat-component-piyion");
+        chatComponent.id = "chatComponent";
+        chatComponent.style.zIndex = "10";
+        chatComponent.setAttribute("position", "left");
+        chatComponent.setAttribute("company", companyId);
+  
+        document.body.appendChild(chatComponent);
+        console.log("Chat component añadido al DOM.");
+      });
+  
     } catch (error) {
       console.error("Error al obtener el Company ID:", error);
     }
